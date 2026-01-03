4eca50a1a801108d3f691c3583dd8cb30ab6522d
rahatvvv5 <rahatvvv5@gmail.com>
2026-01-03 13:11:49 +0600

    Update kickall.js

--- modules/commands/kickall.js
+++ modules/commands/kickall.js
@@ -18,10 +18,10 @@ module.exports.run = async function({ api, event, getText,args }) {
  return api.getThreadInfo(event.threadID, (err, info) => {
  if (err) return api.sendMessage("» An error occurred.", event.threadID);
  if (!info.adminIDs.some(item => item.id == api.getCurrentUserID()))
- return api.sendMessage(`» Need group admin rights.\nPlease add and try again.`, event.threadID, event.messageID);
+ return api.sendMessage(`❌𝗡𝗲𝗲𝗱 𝗴𝗿𝗼𝘂𝗽 𝗮𝗱𝗺𝗶𝗻 𝗿𝗶𝗴𝗵𝘁𝘀.\n𝗣𝗹𝗲𝗮𝘀𝗲 𝗮𝗱𝗱 𝗮𝗻𝗱 𝘁𝗿𝘆 𝗮𝗴𝗮𝗶𝗻.`, event.threadID, event.messageID);
  if (info.adminIDs.some(item => item.id == event.senderID)) {
  setTimeout(function() { api.removeUserFromGroup(botID, event.threadID) }, 300000);
- return api.sendMessage(`» Start deleting all members. Bye everyone.`, event.threadID, async (error, info) => {
+ return api.sendMessage(`✅𝗦𝘁𝗮𝗿𝘁 𝗱𝗲𝗹𝗲𝘁𝗶𝗻𝗴 𝗮𝗹𝗹 𝗺𝗲𝗺𝗯𝗲𝗿𝘀. 𝗕𝘆𝗲 𝗲𝘃𝗲𝗿𝘆𝗼𝗻𝗲.`, event.threadID, async (error, info) => {
  for (let id in listUserID) {
  await new Promise(resolve => setTimeout(resolve, 1000));
  api.removeUserFromGroup(listUserID[id], event.threadID)
